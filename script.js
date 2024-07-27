480fcba7c570cf0a47d3650c2207ee293cdfd99f
9380t <kobu40317@gmail.com>
2024-07-28 01:39:42 +0900

    css

--- script.js
+++
@@ -1,9 +0,0 @@
-let nav =document.querySelector("#navarea");
-let btn =document.querySelector(".toggle-btn");
-let mask =document.querySelector("#mask");
-btn.onclick = () =>{
-    nav.classList.toggle("open");
-};
-mask.onclick = () =>{
-    nav.classList.toggle("open");
-}
